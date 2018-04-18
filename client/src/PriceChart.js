0e72d9bb954d755cf9ff0ca8a2cef2591460e03c
James Reynolds <jameshreynolds@gmail.com>
2018-04-17 23:06:38 -0700

    Data is now passed down from parent component

--- client/src/PriceChart.js
+++ client/src/PriceChart.js
@@ -4,23 +4,98 @@ import { Bar, Line, Pie, Radar } from 'react-chartjs-2';
 class PriceChart extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      data: [{
+        maintainAspectRatio:true,
+        labels: null,
+        datasets: [
+          {
+            label: 'Left Eye CC',
+            type:'line',
+            // yAxisID: 'A',
+            fill: false,
+            data: null,
+            backgroundColor: 'rgba(188, 169, 255, 0.7)',
+            borderColor: 'rgba(188, 169, 255, 0.7)',
+            borderWidth: 10
+          },
+          {
+            label: 'Right Eye CC',
+            type:'line',
+            // yAxisID: 'A',
+            fill: false,
+            data: null,
+            borderColor: 'rgba(231, 236, 163, 0.7)',
+            borderWidth: 10
+          },
+          {
+            label: 'Blood pressure x 10',
+            type:'line',
+            // yAxisID: 'A',
+            fill: false,
+            data: null,
+            borderColor: 'rgba(137, 7, 180, 0.6)',
+            borderWidth: 10
+          },
+          {
+            label: 'Dose data -50',
+            //yAxisID: 'B',
+            data: null,
+            backgroundColor: 'rgba(103, 161, 173, 0.9)',
+            borderWidth: 0
+          }
+        ]
+      }],
+      readyToRender: false
+    }
   }
   
+  updateData() {
+    let currentData = this.state.data.slice();
+    currentData[0].labels = this.props.update.timeScale;
+    currentData[0].datasets[0].data = this.props.update.leftEye;
+    currentData[0].datasets[1].data = this.props.update.rightEye;
+    currentData[0].datasets[2].data = this.props.update.bloodPressure;
+    currentData[0].datasets[3].data = this.props.update.dose;
+    this.setState({
+      data: currentData,
+      readyToRender: true
+    })
+  }
+
+  componentWillMount() {
+    this.updateData();
+  }
 
   render() {
     console.log('props', this.props);
-    return (
-      <div className='chart'>
-        Price Chart
-        <Bar
-          data={this.props.data}
-          width={100}
-          height={30}
-          
-        />
-      </div>
-    );
+    if (this.state.readyToRender) {
+      return (
+        <div className='chart'>
+          Wellness Progress
+          <Bar
+            data={this.state.data[0]}
+            width={100}
+            height={30}
+            options={
+              {
+                scales: {
+                  yAxes: [{
+                    ticks: {
+                        beginAtZero:false
+                    }
+                  }]
+                }
+              }
+            }
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div>...Loading</div>
+      )
+    }
   }
 }
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,129 +10,11 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {
-        chartData: {
-          maintainAspectRatio:false,
-          labels: ["Red", "Blue", "Yellow", "Green"],
-          datasets: [
-            {
-              label: 'Left eye chart data',
-              type:'line',
-              // yAxisID: 'A',
-              fill: false,
-              data: [17.4, 18.5, 17.5, 19.2, 16.9],
-              backgroundColor: 'rgba(188, 169, 255, 0.7)',
-              // backgroundColor: [
-              //     'rgba(255, 99, 132, 0.2)',
-              //     'rgba(54, 162, 235, 0.2)',
-              //     'rgba(255, 206, 86, 0.2)',
-              //     'rgba(75, 192, 192, 0.2)',
-              //     'rgba(153, 102, 255, 0.2)',
-              //     'rgba(255, 159, 64, 0.2)'
-              // ],
-              borderColor: 'rgba(188, 169, 255, 0.7)',
-              // borderColor: 
-              // [
-              //     'rgba(255,99,132,1)',
-              //     'rgba(54, 162, 235, 1)',
-              //     'rgba(255, 206, 86, 1)',
-              //     'rgba(75, 192, 192, 1)',
-              //     'rgba(153, 102, 255, 1)',
-              //     'rgba(255, 159, 64, 1)'
-              // ],
-              borderWidth: 10
-          },
-          {
-            label: 'Right Eye chart data',
-            type:'line',
-            // yAxisID: 'A',
-            fill: false,
-            data: [17.5, 17.5, 19.5, 16.2, 16.4],
-            // backgroundColor: 'rgba(231, 236, 163, 0.9)',
-            //     'rgba(255, 99, 132, 0.2)',
-            //     'rgba(54, 162, 235, 0.2)',
-            //     'rgba(255, 206, 86, 0.2)',
-            //     'rgba(75, 192, 192, 0.2)',
-            //     'rgba(153, 102, 255, 0.2)',
-            //     'rgba(255, 159, 64, 0.2)'
-            // ],
-            borderColor: 'rgba(231, 236, 163, 0.7)',
-            // [
-            //     'rgba(255,99,132,1)',
-            //     'rgba(54, 162, 235, 1)',
-            //     'rgba(255, 206, 86, 1)',
-            //     'rgba(75, 192, 192, 1)',
-            //     'rgba(153, 102, 255, 1)',
-            //     'rgba(255, 159, 64, 1)'
-            // ],
-            borderWidth: 10
-          },
-          {
-            label: 'Blood pressure chart data',
-            type:'line',
-            // yAxisID: 'A',
-            fill: false,
-            data: [158/81 *10, 150/90 *10, 152/91 *10, 150/91 *10, 157/80 *10],
-            // backgroundColor: 'rgba(243, 134, 48, 0.8)',
-            // [
-            //     'rgba(255, 99, 132, 0.2)',
-            //     'rgba(54, 162, 235, 0.2)',
-            //     'rgba(255, 206, 86, 0.2)',
-            //     'rgba(75, 192, 192, 0.2)',
-            //     'rgba(153, 102, 255, 0.2)',
-            //     'rgba(255, 159, 64, 0.2)'
-            // ],
-            borderColor: 'rgba(137, 7, 180, 0.6)',
-            // [
-            //     'rgba(255,99,132,1)',
-            //     'rgba(54, 162, 235, 1)',
-            //     'rgba(255, 206, 86, 1)',
-            //     'rgba(75, 192, 192, 1)',
-            //     'rgba(153, 102, 255, 1)',
-            //     'rgba(255, 159, 64, 1)'
-            // ],
-            borderWidth: 10
-          },
-          {
-            label: 'Dose chart data',
-            // yAxisID: 'B',
-            // fillColor: 'rgba(157, 255, 154, 1)',
-            // strokeColor: 'rgba(157, 255, 154, 1)',
-            data: [65 -50, 70  -50, 69  -50, 68  -50, 72  -50],
-            backgroundColor: 'rgba(103, 161, 173, 0.9)',
-            //[
-            //     'rgba(157, 255, 154, 1)'
-            // ],
-            // borderColor: [
-            //     'rgba(255,99,132,1)',
-            //     'rgba(54, 162, 235, 1)',
-            //     'rgba(255, 206, 86, 1)',
-            //     'rgba(75, 192, 192, 1)',
-            //     'rgba(153, 102, 255, 1)',
-            //     'rgba(255, 159, 64, 1)'
-            // ],
-            borderWidth: 0
-          }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            yAxisID: 'A',
-            // type: 'linear',
-            position: 'left',
-          }, {
-            yAxisID: 'B',
-            type: 'linear',
-            position: 'right',
-            ticks: {
-              max: 1,
-              min: 0
-            }
-          }]
-        }
-        }
-      }
+      timeScale: ['3/27', '3/28', '3/29', '3/30', '4/1', '3/27', '3/28', '3/29', '3/30', '4/1'],
+      leftEye: [17.4, 18.5, 17.5, 19.2, 16.9, 17.4, 18.5, 17.5, 19.2, 16.9],
+      rightEye: [17.5, 17.5, 19.5, 16.2, 16.4, 17.5, 17.5, 19.5, 16.2, 16.4],
+      bloodPressure: [158/81 *10, 150/90 *10, 152/91 *10, 150/91 *10, 157/80 *10, 158/81 *10, 150/90 *10, 152/91 *10, 150/91 *10, 157/80 *10],
+      dose: [65 -50, 70  -50, 69  -50, 68  -50, 72  -50, 65 -50, 70  -50, 69  -50, 68  -50, 72  -50],
     };
   }
 
@@ -211,16 +93,16 @@ class App extends React.Component {
   // }
   render() {
     console.log('App state',this.state);
-    if (this.state.data) {
+    // if (this.state.data) {
       return (
-        <PriceChart data={this.state.data.chartData} />
+        <PriceChart update={this.state}/>
       );
-    } else {
-      return (
-        <div>...Loading</div>
-      )
+    // } else {
+    //   return (
+    //     <div>...Loading</div>
+    //   )
       
-    }
+    // }
     
   }
 }
